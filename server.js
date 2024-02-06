e174763eed6361d9942f097110281b6d90f5b218
Sourav <sourav862888@gmail.com>
2024-02-06 20:58:07 +0530

    Completed pdf merger

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+import mergePdfs from "./merge.js";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// const express = require("express");
+// const multer = require("multer");
+// const { mergePdfs } = require("./merge");
+
+const app = express();
+
+const upload = multer({ dest: "uploads/" });
+app.use("/static", express.static("public"));
+// const path = require("path");
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "templates/index.html"));
+});
+
+// app.post("/", (req, res) => {});
+app.post("/merge", upload.array("pdfs", 12), async function (req, res, next) {
+  console.log(req.files);
+  await mergePdfs(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path)
+  );
+  res.redirect("http://localhost:3000/static/merged.pdf");
+  // res.send({ data: req.files });
+  // req.files is array of `photos` files
+  // req.body will contain the text fields, if there were any
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
